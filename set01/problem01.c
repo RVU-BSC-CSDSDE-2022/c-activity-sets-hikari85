0e832450b3c5c5744b33ab0e9cb4351a3cc322de
Prabhas Bhat <>
2022-09-27 11:07:46 +0000

    changed the number

---
+++ set01/problem01.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{
+  printf("Prabhas Bhat");
+  return(0);
+}
---
+++ set01/problem02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+  int a=6,b=10,sum;
+  sum=a+b;
+  printf("The sum of the 2 numbers is %d",sum);
+  return(0);
+}
